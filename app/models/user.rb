31a9b8248298662ed7a29d9fe56f8ad1bb22e34b
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-09 16:50:02 -0700

    Edit devise sign_up to allow username param

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  validates :username, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :username, presence: true
-  validates :username, uniqueness: true
 end
