a84208757200e2426727356ca65e79f4eec95752
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-10 11:27:33 -0700

    Add list of attendees per event

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,5 +4,5 @@ class Event < ApplicationRecord
 
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   has_many :attendances
-  has_many :attendees, through: :attendances
+  has_many :attendee, through: :attendances, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   has_many :events, inverse_of: 'creator'
   has_many :attendances
-  has_many :attended_events, through: :attendances
+  has_many :attended_events, through: :attendances, source: :event
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
