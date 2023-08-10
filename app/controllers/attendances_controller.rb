970340ae349a7e2fd6e0937bcd3fbe091d53cb37
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-09 21:34:33 -0700

    Add more conditions to attendance #create
    Made sure that users can only join the events
    once

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,19 @@
 class AttendancesController < ApplicationController
   before_action :authenticate_user!
+
   def create
+    event_id = params[:event_id]
+    @attendance = current_user.attendances.new(event_id: event_id)
+    if Attendance.where.not(["event_id = ?", event_id]).present?
+      flash[:notice] = "You've already joined this event."
+      redirect_to root_path
+    elsif @attendance.save
+      flash[:notice] = 'Event added!'
+      redirect_to root_path
+    else
+      flash[:error] = 'Failed to create event!'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
